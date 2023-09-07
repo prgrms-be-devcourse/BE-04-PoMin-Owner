2163ce81cbcd524bd8e68c1b59cbc7efc72f23cd
junho <ssmm0205@naver.com>
2023-09-07 13:25:23 +0900

    feat: initial project setting

---
+++ pomin-owner/src/main/java/com/ray/pominowner/PominOwnerApplication.java
@@ -0,0 +1,13 @@
+package com.ray.pominowner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PominOwnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PominOwnerApplication.class, args);
+    }
+
+}
