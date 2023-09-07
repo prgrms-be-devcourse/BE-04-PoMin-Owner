32051303fc84fd8ace63597f2f8d8a0f11e5ee33
junho <ssmm0205@naver.com>
2023-09-07 17:01:44 +0900

    refactor: wildcard 제거

--- src/main/java/com/ray/pominowner/menu/domain/MenuOptionGroup.java
+++ src/main/java/com/ray/pominowner/menu/domain/MenuOptionGroup.java
@@ -1,7 +1,12 @@
 package com.ray.pominowner.menu.domain;
 
 import com.ray.pominowner.global.domain.BaseTimeEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 import static jakarta.persistence.FetchType.LAZY;
 
