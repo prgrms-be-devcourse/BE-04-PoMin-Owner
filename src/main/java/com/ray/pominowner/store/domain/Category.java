daaa9c9e67427d4c11d8f7b4b12011300cd86e46
junho <ssmm0205@naver.com>
2023-09-08 13:58:04 +0900

    feat: 초키 카테고리 정보 캐싱 기능 구현

--- src/main/java/com/ray/pominowner/store/domain/Category.java
+++ src/main/java/com/ray/pominowner/store/domain/Category.java
@@ -5,9 +5,25 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseTimeEntity {
+
+    public static List<Category> INITIAL_CATEGORY_CACHE;
+
+    static {
+        INITIAL_CATEGORY_CACHE = Arrays.stream(CategoryInfo.values())
+                .map(categoryInfo -> new Category(categoryInfo.name(), categoryInfo.url()))
+                .collect(Collectors.toList());
+    }
 
     @Id
     @Column(name = "CATEGORY_ID")
@@ -18,4 +34,13 @@ public class Category extends BaseTimeEntity {
 
     private String image;
 
+    @Builder
+    private Category(final String name, final String image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public static List<Category> initialList() {
+        return List.copyOf(INITIAL_CATEGORY_CACHE);
+    }
 }
---
+++ src/main/java/com/ray/pominowner/store/domain/CategoryInfo.java
@@ -0,0 +1,27 @@
+package com.ray.pominowner.store.domain;
+
+public enum CategoryInfo {
+    한식,
+    분식,
+    카페_디저트,
+    돈까스_회_일식,
+    치킨,
+    피자,
+    아시안_양식,
+    중국집,
+    족발_보쌈,
+    야식,
+    찜_탕,
+    도시락,
+    패스트푸드,
+    프랜차이즈,
+    ;
+
+    private static final String IMAGE_PREFIX = "src/main/resources/categoryimage/";
+    private static final String SUFFIX = ".png";
+
+    public String url() {
+        return IMAGE_PREFIX + this.name() + SUFFIX;
+    }
+
+}
