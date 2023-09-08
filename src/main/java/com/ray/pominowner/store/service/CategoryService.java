bf6670058683a3a289901c34a0bc71dc736b57bb
junho <ssmm0205@naver.com>
2023-09-08 13:59:17 +0900

    feat: 카테고리 레포지토리 생성 및 Bean 생성 후 실행 메서드 추가

---
+++ src/main/java/com/ray/pominowner/store/service/CategoryService.java
@@ -0,0 +1,30 @@
+package com.ray.pominowner.store.service;
+
+
+import com.ray.pominowner.store.domain.Category;
+import com.ray.pominowner.store.repository.CategoryRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    @PostConstruct
+    @Transactional
+    public void afterLoadingApplication() {
+        log.info("called afterPropertiesSet method");
+        List<Category> initialCategories = Category.initialList();
+        repository.saveAll(initialCategories);
+    }
+
+}
