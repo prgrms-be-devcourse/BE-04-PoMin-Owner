6256e557cc39d3bc5ebbdb29350301bbf5ca4e67
junho <ssmm0205@naver.com>
2023-09-08 18:04:14 +0900

    chore: 불필요한 의존성 제거

--- src/main/java/com/ray/pominowner/global/config/category/CategoryCache.java
+++ src/main/java/com/ray/pominowner/global/config/category/CategoryCache.java
@@ -1,10 +1,6 @@
 package com.ray.pominowner.global.config.category;
 
 import com.ray.pominowner.store.domain.Category;
-import com.ray.pominowner.store.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
