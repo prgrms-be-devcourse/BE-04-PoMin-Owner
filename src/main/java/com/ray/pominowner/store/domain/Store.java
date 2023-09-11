aefe1036d23422f8d1e04817c7ae7ed33dee5ab9
junho <ssmm0205@naver.com>
2023-09-11 14:26:30 +0900

    feat: Store 값 객체 분리 및 validation 로직 추가

--- src/main/java/com/ray/pominowner/store/domain/Store.java
+++ src/main/java/com/ray/pominowner/store/domain/Store.java
@@ -2,32 +2,47 @@ package com.ray.pominowner.store.domain;
 
 import com.ray.pominowner.global.domain.BaseTimeEntity;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Store extends BaseTimeEntity {
+
+    private static final String DEFAULT_VALUE = "NOT_SELECTED";
 
     @Id
     @Column(name = "STORE_ID")
     @GeneratedValue
     private Long id;
 
-    private Long businessNumber;
+    @Embedded
+    private RequiredStoreInfo requiredStoreInfo;
 
-    private String name;
+    @Column(columnDefinition = "TEXT default 'NOT_SELECTED'")
+    private String info = DEFAULT_VALUE;    // 선택
 
-    private String info;
+    @Column(columnDefinition = "TEXT default 'NOT_SELECTED'")
+    private String tel = DEFAULT_VALUE;    // 선택
 
-    private String tel;
+    private Long ownerId;   // 추후 연관관계 설정 예정
 
-    private String address;
+    public Store(final RequiredStoreInfo requiredStoreInfo) {
+        validateRequiredInfo(requiredStoreInfo);
+        this.requiredStoreInfo = requiredStoreInfo;
+    }
 
-    private String logoImage;
+    private void validateRequiredInfo(final RequiredStoreInfo requiredStoreInfo) {
+        Assert.notNull(requiredStoreInfo, "가게 정보는 필수 입니다.");
+    }
 
-    private Long ownerId;
-
-    // StoreCategory 양방향 매핑
+    public Long getId() {
+        return id;
+    }
 
 }
