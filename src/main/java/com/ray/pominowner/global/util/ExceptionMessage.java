a4d418cc2770b2a522385203dfe5416d0b1a5e8f
hyeon-z <phj8221@naver.com>
2023-09-11 14:34:21 +0900

    feat: 정산 엔티티의 @Embedded 객체 유효성 검사

--- src/main/java/com/ray/pominowner/global/util/ExceptionMessage.java
+++ src/main/java/com/ray/pominowner/global/util/ExceptionMessage.java
@@ -10,7 +10,10 @@ public enum ExceptionMessage {
     INVALID_AMOUNT("금액은 0이상의 값이어야 합니다."),
     NULL_PAYMENT_STATUS("결제 상태는 필수 값입니다."),
     NULL_PAYMENT_PROVIDER("PG사는 필수 값입니다."),
-    INVALID_PAYOUT_DATE("지급일은 매출일과같거나 그보다 늦어야 합니다.");
+    INVALID_PAYOUT_DATE("지급일은 매출일과같거나 그보다 늦어야 합니다."),
+    NULL_FEE_OBJECT("수수료 관련 값은 필수입니다."),
+    NULL_PAYOUT_OBJECT("지급 관련 값은 필수입니다."),
+    NULL_SALES_OBJECT("매출 관련 값은 필수입니다.");
 
     private final String message;
 }
