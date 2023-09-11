92edc78c65cc1a07c6107b7e46b103c96a34bf3b
junho <ssmm0205@naver.com>
2023-09-11 14:29:03 +0900

    test: Store 등록 api 테스트 작성

---
+++ src/test/java/com/ray/pominowner/store/controller/StoreControllerTest.java
@@ -0,0 +1,53 @@
+package com.ray.pominowner.store.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ray.pominowner.store.controller.dto.StoreRegisterRequest;
+import com.ray.pominowner.store.domain.Store;
+import com.ray.pominowner.store.service.StoreService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StoreController.class)
+class StoreControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private StoreService storeService;
+
+    @Test
+    @WithMockUser
+    @DisplayName("가게 등록에 성공한다")
+    void successRegisterStore() throws Exception {
+        // given
+        StoreRegisterRequest storeRegisterRequest = new StoreRegisterRequest(1234567890L, "가게이름", "서울특별시 가게동 주소구", "가게이미지URL.png");
+        given(storeService.registerStore(any(Store.class)))
+                .willReturn(1L);
+
+        // when, then
+        this.mvc.perform(post("/api/v1/stores")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(storeRegisterRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(header().stringValues("Location", "/api/v1/stores/1"));
+    }
+
+}
