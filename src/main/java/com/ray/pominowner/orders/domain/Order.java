20efb3df5b476047a011dd16828f55e3a0f6de93
Choi Seongwon <vlflalektzja@naver.com>
2023-09-11 13:56:17 +0900

    feat: Order에 휴대폰번호 객체 추가, 빌더 추가

--- src/main/java/com/ray/pominowner/orders/domain/Order.java
+++ src/main/java/com/ray/pominowner/orders/domain/Order.java
@@ -1,19 +1,28 @@
 package com.ray.pominowner.orders.domain;
 
 import com.ray.pominowner.global.domain.BaseTimeEntity;
+import com.ray.pominowner.global.domain.PhoneNumber;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.EnumType.STRING;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Table(name = "ORDERS")
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
 public class Order extends BaseTimeEntity {
 
     @Id
@@ -21,9 +30,9 @@ public class Order extends BaseTimeEntity {
     @GeneratedValue
     private Long id;
 
-    private String orderNumber;
+    private String orderNumber; // 주문 번호
 
-    private Integer receiptNumber;
+    private Integer receiptNumber; // 접수 번호
 
     @Enumerated(value = STRING)
     private OrderStatus status;
@@ -32,11 +41,12 @@ public class Order extends BaseTimeEntity {
 
     private Integer totalPrice;
 
-    private String customerPhoneNumber;
+    @Embedded
+    private PhoneNumber customerPhoneNumber;
+
+    private LocalDateTime reservationTime;
 
     // STORE 와 다대일 매핑
     private Long storeId;
-
-    private LocalDateTime reservationTime;
 
 }
