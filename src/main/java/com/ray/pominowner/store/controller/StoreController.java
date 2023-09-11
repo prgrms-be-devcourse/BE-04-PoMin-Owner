7911fc7630a289351789e6ee7d97737c75c8e7d0
junho <ssmm0205@naver.com>
2023-09-11 14:28:12 +0900

    feat: 필수 정보를 통한 가게 등록 기능 구현

---
+++ src/main/java/com/ray/pominowner/store/controller/StoreController.java
@@ -0,0 +1,29 @@
+package com.ray.pominowner.store.controller;
+
+import com.ray.pominowner.store.controller.dto.StoreRegisterRequest;
+import com.ray.pominowner.store.service.StoreService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController()
+@RequestMapping("/api/v1/stores")
+@RequiredArgsConstructor
+public class StoreController {
+
+    private final StoreService storeService;
+
+    @PostMapping()
+    public ResponseEntity<Void> registerStore(@RequestBody @Valid StoreRegisterRequest request) {
+
+        Long storeId = storeService.registerStore(request.toEntity());
+        return ResponseEntity.created(URI.create("/api/v1/stores/" + storeId)).build();
+    }
+
+}
