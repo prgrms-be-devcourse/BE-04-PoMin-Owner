a3712981079c807ecc3ca7b2ab2ce46116b976b4
hyeon-z <phj8221@naver.com>
2023-09-11 13:56:53 +0900

    feat: 정산 지급일 유효성 검사

--- src/main/java/com/ray/pominowner/settlement/domain/PayOut.java
+++ src/main/java/com/ray/pominowner/settlement/domain/PayOut.java
@@ -3,6 +3,7 @@ package com.ray.pominowner.settlement.domain;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 import static com.ray.pominowner.global.util.ExceptionMessage.INVALID_AMOUNT;
 import static com.ray.pominowner.global.util.Validator.validate;
 
+@Getter
 @Embeddable
 @EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
