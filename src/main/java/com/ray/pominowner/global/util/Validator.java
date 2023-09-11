2adb5d528439d88ec79aa75a6952dca79169c174
hyeon-z <phj8221@naver.com>
2023-09-11 14:00:14 +0900

    refactor: Validator 클래스의 조건 네이밍 변경

--- src/main/java/com/ray/pominowner/global/util/Validator.java
+++ src/main/java/com/ray/pominowner/global/util/Validator.java
@@ -6,8 +6,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Validator {
 
-    public static void validate(boolean condition, ExceptionMessage exception) {
-        if (condition) {
+    public static void validate(boolean failureCondition, ExceptionMessage exception) {
+        if (failureCondition) {
             throw new IllegalArgumentException(exception.getMessage());
         }
     }
