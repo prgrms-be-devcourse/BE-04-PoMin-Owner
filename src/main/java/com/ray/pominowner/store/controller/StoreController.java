6a4811991fcd64140f9c1213cd498df7637cb4ff
junho <ssmm0205@naver.com>
2023-09-12 15:14:46 +0900

    test: StoreService 테스트 작성

--- src/main/java/com/ray/pominowner/store/controller/StoreController.java
+++ src/main/java/com/ray/pominowner/store/controller/StoreController.java
@@ -6,13 +6,11 @@ import com.ray.pominowner.store.controller.dto.StoreRegisterRequest;
 import com.ray.pominowner.store.service.StoreService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
