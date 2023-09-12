1c41f45fee2b8213b5df01fa658609fc80a45c38
hyeon-z <phj8221@naver.com>
2023-09-12 17:17:51 +0900

    feat: PG사 수수료 로직 구현

--- src/main/java/com/ray/pominowner/payment/domain/PGType.java
+++ src/main/java/com/ray/pominowner/payment/domain/PGType.java
@@ -1,7 +1,20 @@
 package com.ray.pominowner.payment.domain;
 
+import java.util.function.Function;
+
 public enum PGType {
-    TOSS,
-    NHN,
-    NICE
+    TOSS(value -> value * 0.03),
+    NHN(value -> value * 0.029),
+    NICE(value -> value * 0.035);
+
+    private final Function<Integer, Double> expression;
+
+    PGType(Function<Integer, Double> expression) {
+        this.expression = expression;
+    }
+
+    public int calculate(int value) {
+        double apply = expression.apply(value);
+        return (int) apply;
+    }
 }
