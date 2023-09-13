7e91124f151348da9577292e0f6da05496f5801e
junho <ssmm0205@naver.com>
2023-09-13 16:52:32 +0900

    test: token을 올바르게 가지고 오는지 검증

---
+++ src/test/java/com/ray/pominowner/global/config/TokenProviderTest.java
@@ -0,0 +1,29 @@
+package com.ray.pominowner.global.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+class TokenProviderTest {
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Test
+    @DisplayName("yml에 설정된 올바른 토큰을 가져올 수 있다.")
+    void successGetCorrectToken() {
+        String businessNumberServiceKey = tokenProvider.getBusinessNumberServiceKey();
+        String businessNumberHeaderAuthKey = tokenProvider.getBusinessNumberHeaderAuthKey();
+        log.info("businessNumberServiceKey: {}, businessNumberHeaderAuthKey: {}", businessNumberServiceKey, businessNumberHeaderAuthKey);
+
+        assertThat(businessNumberServiceKey).isNotBlank().isNotEqualTo("null");
+        assertThat(businessNumberHeaderAuthKey).isNotBlank().isNotEqualTo("null");
+    }
+
+}
