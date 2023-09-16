cad82c02185254da20280ccd0d4598c0efc948fb
junho <ssmm0205@naver.com>
2023-09-17 01:45:14 +0900

    feat: 가게 이미지 repository 추가

---
+++ src/main/java/com/ray/pominowner/store/repository/StoreImageRepository.java
@@ -0,0 +1,8 @@
+package com.ray.pominowner.store.repository;
+
+import com.ray.pominowner.store.domain.StoreImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreImageRepository extends JpaRepository<StoreImage, Long> {
+
+}
