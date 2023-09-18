2adb5a7ae04841d4cb43e2cb0c0274f9e4acbfea
hyeon-z <phj8221@naver.com>
2023-09-18 17:35:04 +0900

    feat: 주문 id에 따른 정산 정보 찾는 서비스 메서드 구현

--- src/main/java/com/ray/pominowner/settlement/service/SettlementService.java
+++ src/main/java/com/ray/pominowner/settlement/service/SettlementService.java
@@ -1,10 +1,18 @@
 package com.ray.pominowner.settlement.service;
 
+import com.ray.pominowner.orders.domain.Order;
+import com.ray.pominowner.orders.domain.OrderStatus;
+import com.ray.pominowner.orders.repository.OrderRepository;
+import com.ray.pominowner.settlement.controller.dto.SettlementResponse;
 import com.ray.pominowner.settlement.domain.Settlement;
 import com.ray.pominowner.settlement.repository.SettlementRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.ray.pominowner.global.util.ExceptionMessage.INVALID_ORDER;
+import static com.ray.pominowner.global.util.ExceptionMessage.NO_ORDER;
+import static com.ray.pominowner.global.util.ExceptionMessage.NO_SETTLEMENT;
 
 @Service
 @Transactional
@@ -13,8 +21,28 @@ public class SettlementService {
 
     private final SettlementRepository settlementRepository;
 
+    private final OrderRepository orderRepository;
+
     public Settlement create(Settlement settlement) {
         return settlementRepository.save(settlement);
+    }
+
+    public SettlementResponse getSettlementByOrder(Long orderId) {
+        validateOrder(orderId);
+
+        Settlement settlement = settlementRepository.findSettlementByOrderIdAndDeleted(orderId, false)
+                .orElseThrow(() -> new IllegalArgumentException(NO_SETTLEMENT.getMessage()));
+
+        return new SettlementResponse(settlement);
+    }
+
+    private void validateOrder(Long orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new IllegalArgumentException(NO_ORDER.getMessage()));
+
+        if (order.getStatus() == OrderStatus.CANCELED || order.getStatus() == OrderStatus.REJECTED) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+        }
     }
 
 }
