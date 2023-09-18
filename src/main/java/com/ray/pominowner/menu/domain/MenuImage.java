66a2310047ca0687508d4209704d0bc9773ee81b
junho <ssmm0205@naver.com>
2023-09-18 14:59:39 +0900

    feat: 메뉴 도메인 엔티티 생성

---
+++ src/main/java/com/ray/pominowner/menu/domain/MenuImage.java
@@ -0,0 +1,34 @@
+package com.ray.pominowner.menu.domain;
+
+import com.ray.pominowner.global.domain.BaseFileEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MenuImage extends BaseFileEntity {
+
+    @Id
+    @Column(name = "MENU_IMAGE_ID")
+    @GeneratedValue
+    private Long id;
+
+    @Builder
+    private MenuImage(String path, String uploadName, String fileName) {
+        validateImage(path, uploadName, fileName);
+
+        this.path = path;
+        this.uploadName = uploadName;
+        this.fileName = fileName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
