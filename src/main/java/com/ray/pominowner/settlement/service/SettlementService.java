23eb4245bb128fd67ddc55d0a493ce09e88367f2
hyeon-z <phj8221@naver.com>
2023-09-19 15:26:25 +0900

    refactor: Service의 validate코드를 Order 엔티티의 메서드로 이동

--- src/main/java/com/ray/pominowner/settlement/service/SettlementService.java
+++ src/main/java/com/ray/pominowner/settlement/service/SettlementService.java
@@ -1,7 +1,5 @@
 package com.ray.pominowner.settlement.service;
 
-import com.ray.pominowner.orders.domain.Order;
-import com.ray.pominowner.orders.domain.OrderStatus;
 import com.ray.pominowner.orders.repository.OrderRepository;
 import com.ray.pominowner.settlement.controller.dto.SettlementResponse;
 import com.ray.pominowner.settlement.domain.Settlement;
@@ -10,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.ray.pominowner.global.util.ExceptionMessage.INVALID_ORDER;
 import static com.ray.pominowner.global.util.ExceptionMessage.NO_ORDER;
 import static com.ray.pominowner.global.util.ExceptionMessage.NO_SETTLEMENT;
 
@@ -28,21 +25,14 @@ public class SettlementService {
     }
 
     public SettlementResponse getSettlementByOrder(Long orderId) {
-        validateOrder(orderId);
+        orderRepository.findById(orderId)
+                .orElseThrow(() -> new IllegalArgumentException(NO_ORDER.getMessage()))
+                .validateStatus();
 
         Settlement settlement = settlementRepository.findSettlementByOrderIdAndDeleted(orderId, false)
                 .orElseThrow(() -> new IllegalArgumentException(NO_SETTLEMENT.getMessage()));
 
         return new SettlementResponse(settlement);
-    }
-
-    private void validateOrder(Long orderId) {
-        Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new IllegalArgumentException(NO_ORDER.getMessage()));
-
-        if (order.getStatus() == OrderStatus.CANCELED || order.getStatus() == OrderStatus.REJECTED) {
-            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
-        }
     }
 
 }
