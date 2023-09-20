f756d30d03212fd024318fb979dc5d2cc9725f77
junho <ssmm0205@naver.com>
2023-09-20 15:49:41 +0900

    feat: 옵션에 대한 도메인 로직 작성

--- src/main/java/com/ray/pominowner/menu/domain/Option.java
+++ src/main/java/com/ray/pominowner/menu/domain/Option.java
@@ -5,8 +5,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = PROTECTED)
 public class Option extends BaseTimeEntity {
 
     @Id
@@ -18,6 +30,41 @@ public class Option extends BaseTimeEntity {
 
     private int price;
 
-    private Long optionGroupId;
+    private boolean selected;
+
+    @ManyToOne
+    @JoinColumn(name = "OPTION_GROUP_ID")
+    private OptionGroup optionGroup;
+
+    @Builder
+    private Option(Long id, String name, int price, boolean selected, OptionGroup optionGroup) {
+        validateConstructor(name, price, optionGroup);
+
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.selected = selected;
+        this.optionGroup = optionGroup;
+    }
+
+    private void validateConstructor(String name, int price, OptionGroup optionGroup) {
+        Assert.hasText(name, "옵션 이름은 필수입니다.");
+        Assert.isTrue(price >= 0, "잘못된 가격을 입력하셨습니다.");
+        Assert.notNull(optionGroup, "옵션 그룹 선택은 필수입니다.");
+    }
+
+    public Option changeSelectionStatus(boolean selected) {
+        if (this.selected == selected) {
+           return this;
+        }
+
+        return Option.builder()
+                .id(this.id)
+                .name(this.name)
+                .price(this.price)
+                .selected(selected)
+                .optionGroup(this.optionGroup)
+                .build();
+    }
 
 }
--- src/main/java/com/ray/pominowner/menu/domain/OptionGroup.java
+++ src/main/java/com/ray/pominowner/menu/domain/OptionGroup.java
@@ -5,8 +5,22 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.FetchType.EAGER;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = PROTECTED)
 public class OptionGroup extends BaseTimeEntity {
 
     @Id
@@ -22,6 +36,60 @@ public class OptionGroup extends BaseTimeEntity {
 
     private Long storeId;
 
-    // Option과 양방향 매핑
+    @OneToMany(mappedBy = "optionGroup", fetch = EAGER, orphanRemoval = true, cascade = PERSIST)
+    private final List<Option> options = new ArrayList<>();
+
+    @Builder
+    private OptionGroup(Long id, String name, int maxOptionCount, boolean required, Long storeId) {
+        validateConstructor(name, maxOptionCount);
+
+        this.id = id;
+        this.name = name;
+        this.maxOptionCount = maxOptionCount;
+        this.required = required;
+        this.storeId = storeId;
+    }
+
+    private void validateConstructor(String name, int maxOptionCount) {
+        Assert.hasText(name, "옵션 그룹 이름은 필수입니다.");
+        Assert.isTrue(maxOptionCount >= 0, "최대 옵션 개수는 0 이상이어야 합니다.");
+    }
+
+    public void add(Option option) {
+        Option changeOptionGroup = Option.builder()
+                .id(option.getId())
+                .name(option.getName())
+                .price(option.getPrice())
+                .selected(option.isSelected())
+                .optionGroup(this)
+                .build();
+
+        this.options.add(changeOptionGroup);
+        checkOptionCount();
+    }
+
+    public int getTotalPrice() {
+        return this.options.stream()
+                .filter(Option::isSelected)
+                .mapToInt(Option::getPrice)
+                .sum();
+    }
+
+    private void checkOptionCount() {
+        long totalCheckedOptionCount = this.options.stream().filter(Option::isSelected).count();
+        if (this.maxOptionCount < totalCheckedOptionCount) {
+            throw new IllegalArgumentException("최대 옵션 개수를 초과하였습니다.");
+        }
+    }
+
+    public void checkRequiredOption() {
+        if (this.required && this.options.stream().noneMatch(Option::isSelected)) {
+            throw new IllegalArgumentException("필수 옵션을 선택해주세요.");
+        }
+    }
+
+    public List<Option> getOptions() {
+        return options;
+    }
 
 }
