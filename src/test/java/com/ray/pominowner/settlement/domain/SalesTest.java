7b28bd085bdd116898aa76c3c238f0a1983b5c43
hyeon-z <phj8221@naver.com>
2023-09-20 19:56:57 +0900

    test: 매출일, 지급일 validate 로직 변경 및 SettlementTestFixture 적용

--- src/test/java/com/ray/pominowner/settlement/domain/SalesTest.java
+++ src/test/java/com/ray/pominowner/settlement/domain/SalesTest.java
@@ -3,11 +3,9 @@ package com.ray.pominowner.settlement.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
-import java.util.stream.Stream;
 
 import static com.ray.pominowner.settlement.SettlementTestFixture.sales;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,20 +19,12 @@ class SalesTest {
         assertThat(sales()).isNotNull();
     }
 
-    @ParameterizedTest(name = "[{index}] salesAmount : {0}, orderedAt : {1}")
-    @MethodSource("invalidSales")
+    @ParameterizedTest(name = "[{index}] salesAmount : {0}")
+    @ValueSource(ints = {1000, -1000})
     @DisplayName("필드 값이 유효하지 않은 경우 Sales 생성에 실패한다.")
-    public void failSales(int salesAmount, LocalDate orderedAt) {
-        assertThatThrownBy(() -> new Sales(salesAmount, orderedAt))
+    public void failSales(int salesAmount) {
+        assertThatThrownBy(() -> new Sales(salesAmount, LocalDate.now()))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    static Stream<Arguments> invalidSales() {
-        return Stream.of(
-                Arguments.arguments(1000, LocalDate.now().plusDays(1)),
-                Arguments.arguments(-1000, LocalDate.now()),
-                Arguments.arguments(-1000, LocalDate.now().plusDays(1))
-        );
     }
 
 }
--- src/test/java/com/ray/pominowner/settlement/domain/SettlementTest.java
+++ src/test/java/com/ray/pominowner/settlement/domain/SettlementTest.java
@@ -1,7 +1,5 @@
 package com.ray.pominowner.settlement.domain;
 
-import com.ray.pominowner.payment.domain.PGType;
-import com.ray.pominowner.settlement.SettlementTestFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,17 +9,18 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.time.LocalDate;
 import java.util.stream.Stream;
 
-import static com.ray.pominowner.settlement.SettlementTestFixture.*;
+import static com.ray.pominowner.settlement.SettlementTestFixture.fee;
+import static com.ray.pominowner.settlement.SettlementTestFixture.payOut;
+import static com.ray.pominowner.settlement.SettlementTestFixture.sales;
+import static com.ray.pominowner.settlement.SettlementTestFixture.settlement;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SettlementTest {
 
-    private static final Fee fee = new Fee(PGType.TOSS, 10000);
+    private final PayOut invalidPayOut = new PayOut(10000, fee(), LocalDate.now());
 
-    private static final PayOut payOut = new PayOut(10000, fee, LocalDate.now().plusDays(1));
-
-    private static final Sales sales = new Sales(1000, LocalDate.now());
+    private final Sales invalidSales = new Sales(1000, LocalDate.now().plusDays(5));
 
 
     @Test
@@ -30,7 +29,7 @@ class SettlementTest {
         assertThat(settlement()).isNotNull();
     }
 
-    @ParameterizedTest(name = "[{index}] amount : {0}, date : {1}")
+    @ParameterizedTest(name = "[{index}] fee : {0}, payOut : {1}, sales: {2}")
     @MethodSource("invalidSettlement")
     @DisplayName("필드 값이 유효하지 않으면 Settlement 생성에 실패한다.")
     public void failSettlement(Fee fee, PayOut payOut, Sales sales) {
@@ -53,13 +52,33 @@ class SettlementTest {
     static Stream<Arguments> invalidSettlement() {
         return Stream.of(
                 Arguments.arguments(null, null, null),
-                Arguments.arguments(null, null, sales),
-                Arguments.arguments(null, payOut, null),
-                Arguments.arguments(null, payOut, sales),
-                Arguments.arguments(fee, null, null),
-                Arguments.arguments(fee, null, sales),
-                Arguments.arguments(fee, payOut, null)
+                Arguments.arguments(null, null, sales()),
+                Arguments.arguments(null, payOut(), null),
+                Arguments.arguments(null, payOut(), sales()),
+                Arguments.arguments(fee(), null, null),
+                Arguments.arguments(fee(), null, sales()),
+                Arguments.arguments(fee(), payOut(), null)
         );
+    }
+
+
+    @Test
+    @DisplayName("지급일이 매출일보다 빠르면 Settlement 생성에 실패한다.")
+    public void failSettlementWhenPayOutDateIsBeforeThanSalesDate() {
+        assertThatThrownBy(() ->
+                Settlement.builder()
+                        .id(1L)
+                        .fee(fee())
+                        .payOut(invalidPayOut)
+                        .sales(invalidSales)
+                        .depositStatus(DepositStatus.SCHEDULED)
+                        .serviceType(ServiceType.PACKAGING)
+                        .storeId(1L)
+                        .orderId(1L)
+                        .paymentId(1L)
+                        .deleted(false)
+                        .build())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
