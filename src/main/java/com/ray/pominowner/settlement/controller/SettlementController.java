e5860fc0ec46a1983c50ff7e03c195f7fd0c44f8
hyeon-z <phj8221@naver.com>
2023-09-20 13:02:59 +0900

    feat: 일 정산 조회 controller 구현

--- src/main/java/com/ray/pominowner/settlement/controller/SettlementController.java
+++ src/main/java/com/ray/pominowner/settlement/controller/SettlementController.java
@@ -1,6 +1,7 @@
 package com.ray.pominowner.settlement.controller;
 
 import com.ray.pominowner.settlement.controller.dto.SettlementResponse;
+import com.ray.pominowner.settlement.service.DateType;
 import com.ray.pominowner.settlement.service.SettlementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +23,14 @@ public class SettlementController {
     @GetMapping("/byOrder/{orderId}")
     public SettlementResponse getSettlementByOrder(@PathVariable Long orderId) {
         return new SettlementResponse(settlementService.getSettlementByOrder(orderId));
+    }
+
+    @GetMapping("/byStore/{storeId}")
+    public List<SettlementResponse> getDailySettlementByStore(@PathVariable Long storeId, @RequestParam DateType dateType, @RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
+        return settlementService.getDailySettlementByStore(storeId, dateType, startDate, endDate)
+                .stream()
+                .map(SettlementResponse::new)
+                .toList();
     }
 
 }
