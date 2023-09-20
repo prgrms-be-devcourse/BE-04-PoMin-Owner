b4b2e99d57374928700a8259eea73660f725a3bc
hyeon-z <phj8221@naver.com>
2023-09-20 19:54:03 +0900

    refactor: 매출일, 지급일 validate 로직 변경

--- src/main/java/com/ray/pominowner/settlement/domain/PayOut.java
+++ src/main/java/com/ray/pominowner/settlement/domain/PayOut.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 
 import static com.ray.pominowner.global.util.ExceptionMessage.INVALID_AMOUNT;
 import static com.ray.pominowner.global.util.ExceptionMessage.NULL_FEE_OBJECT;
-import static com.ray.pominowner.global.util.ExceptionMessage.PAYOUT_DATE_IS_BEFORE_NOW;
 import static com.ray.pominowner.global.util.Validator.validate;
 
 @Getter
@@ -25,19 +24,18 @@ public class PayOut {
     private LocalDate payOutDate;
 
     public PayOut(int paymentAmount, Fee fee, LocalDate orderedAt) {
-        this.payOutDate = calculatePayoutDate(orderedAt);
-        validatePayOut(paymentAmount, fee, payOutDate);
+        validatePayOut(paymentAmount, fee);
 
         int payOutAmount = paymentAmount - fee.calculateTotalMinusAmount();
         validate(payOutAmount >= 0, INVALID_AMOUNT);  // 추후 처리 예정
 
         this.payOutAmount = payOutAmount;
+        this.payOutDate = calculatePayoutDate(orderedAt);
     }
 
-    private void validatePayOut(int paymentAmount, Fee fee, LocalDate payOutDate) {
+    private void validatePayOut(int paymentAmount, Fee fee) {
         Assert.notNull(fee, NULL_FEE_OBJECT.getMessage());
         validate(paymentAmount >= 0, INVALID_AMOUNT);
-        validate(payOutDate.isAfter(LocalDate.now()) || payOutDate.isEqual(LocalDate.now()), PAYOUT_DATE_IS_BEFORE_NOW);
     }
 
     public LocalDate calculatePayoutDate(LocalDate orderedAt) {
