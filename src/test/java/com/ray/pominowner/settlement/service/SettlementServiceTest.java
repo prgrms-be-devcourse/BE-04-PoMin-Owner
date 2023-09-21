31a27d0106459f2409fad0c41b50fdd564ae6cd0
hyeon-z <phj8221@naver.com>
2023-09-21 16:31:13 +0900

    test: 일 정산 조회 service 테스트 작성

--- src/test/java/com/ray/pominowner/settlement/service/SettlementServiceTest.java
+++ src/test/java/com/ray/pominowner/settlement/service/SettlementServiceTest.java
@@ -7,6 +7,8 @@ import com.ray.pominowner.orders.repository.OrderRepository;
 import com.ray.pominowner.settlement.controller.dto.SettlementResponse;
 import com.ray.pominowner.settlement.domain.Settlement;
 import com.ray.pominowner.settlement.repository.SettlementRepository;
+import com.ray.pominowner.settlement.service.vo.SettlementByStoreRequest;
+import com.ray.pominowner.store.repository.StoreRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,11 +16,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static com.ray.pominowner.global.util.ExceptionMessage.INVALID_ORDER;
 import static com.ray.pominowner.settlement.SettlementTestFixture.settlement;
+import static com.ray.pominowner.store.StoreTestFixture.store;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
@@ -34,6 +39,9 @@ class SettlementServiceTest {
 
     @Mock
     OrderRepository orderRepository;
+
+    @Mock
+    StoreRepository storeRepository;
 
     Order order = Order.builder()
             .id(1L)
@@ -122,5 +130,40 @@ class SettlementServiceTest {
         assertThatThrownBy(() -> settlementService.getSettlementByOrder(order.getId()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    SettlementByStoreRequest request = new SettlementByStoreRequest(
+            settlement().getStoreId(),
+            "SALES",
+            LocalDate.now(),
+            LocalDate.now().plusDays(1)
+    );
+
+    @Test
+    @DisplayName("일 정산 조회에 성공한다.")
+    public void successGetDailySettlementByStore() {
+        // given
+        given(storeRepository.findById(request.storeId())).willReturn(Optional.of(store()));
+
+        List<Settlement> expectedSettlements = List.of(settlement());
+        given(DateType.valueOf(request.dateType()).getSettlements(settlementRepository, request)).willReturn(expectedSettlements);
+
+        // when
+        List<Settlement> settlements = settlementService.getDailySettlementByStore(request);
+
+        // then
+        assertThat(settlements).isEqualTo(expectedSettlements);
+    }
+
+    @Test
+    @DisplayName("가게자 존재하지 않는 일 정산 조회에 실패한다.")
+    public void failGetDailySettlementByStoreWhenNonExistStore() {
+        // given
+        given(storeRepository.findById(request.storeId())).willThrow(IllegalArgumentException.class);
+
+        // when, then
+        assertThatThrownBy(() -> settlementService.getDailySettlementByStore(request))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }
