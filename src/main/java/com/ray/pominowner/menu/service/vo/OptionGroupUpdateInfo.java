d70efa484438b0b324b13a46eb4bfc6ca56c98dc
junho <ssmm0205@naver.com>
2023-09-21 17:10:10 +0900

    feat: optionGroup service 수정 및 vo 생성

--- src/main/java/com/ray/pominowner/menu/service/vo/OptionGroupUpdateInfo.java
+++ src/main/java/com/ray/pominowner/menu/service/vo/OptionGroupUpdateInfo.java
@@ -1,4 +1,4 @@
 package com.ray.pominowner.menu.service.vo;
 
-public record OptionGroupUpdateInfo(String name, int maxOptionCount, boolean required) {
+public record OptionGroupUpdateInfo(String name, int maxOptionCount, boolean required, Long optionGroupId) {
 }
