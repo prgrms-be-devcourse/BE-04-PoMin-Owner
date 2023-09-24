7bda28b66b558d21dee287fccb19ef4c0c147e6d
junho <ssmm0205@naver.com>
2023-09-24 19:03:39 +0900

    test: OptionGroupController Rest docs

--- src/test/java/com/ray/pominowner/menu/controller/OptionGroupControllerTest.java
+++ src/test/java/com/ray/pominowner/menu/controller/OptionGroupControllerTest.java
@@ -2,8 +2,8 @@ package com.ray.pominowner.menu.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ray.pominowner.menu.controller.dto.OptionGroupRequest;
+import com.ray.pominowner.menu.controller.dto.OptionGroupUpdateRequest;
 import com.ray.pominowner.menu.controller.dto.OptionGroupWithOptionResponse;
-import com.ray.pominowner.menu.controller.dto.OptionUpdateRequest;
 import com.ray.pominowner.menu.domain.Option;
 import com.ray.pominowner.menu.domain.OptionGroup;
 import com.ray.pominowner.menu.service.OptionGroupService;
@@ -12,19 +12,34 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
+import static org.springframework.restdocs.payload.JsonFieldType.BOOLEAN;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureRestDocs
 @WebMvcTest(OptionGroupController.class)
 class OptionGroupControllerTest {
 
@@ -66,13 +81,29 @@ class OptionGroupControllerTest {
         // given
         OptionGroupWithOptionResponse response = OptionGroupWithOptionResponse.from(optionGroup);
         given(optionGroupService.getOptionGroup(any(Long.class))).willReturn(optionGroup);
+        Long optionGroupId = 1L;
 
         // when, then
-        mvc.perform(get("/api/v1/option-groups")
-                        .param("optionGroupId", "1")
+        mvc.perform(RestDocumentationRequestBuilders.get("/api/v1/option-groups/{optionGroupId}", optionGroupId)
                         .with(csrf()))
                 .andExpect(status().isOk())
-                .andExpect(content().json(mapper.writeValueAsString(response)));
+                .andExpect(content().json(mapper.writeValueAsString(response)))
+                .andDo(
+                        document("option-group/find",
+                                pathParameters(
+                                        parameterWithName("optionGroupId").description("옵션 아이디")
+                                ),
+                                responseFields(
+                                        fieldWithPath("optionGroup.name").type(STRING).description("옵션 그룹 이름"),
+                                        fieldWithPath("optionGroup.maxOptionCount").type(NUMBER).description("옵션 최대 선택 가능 개수"),
+                                        fieldWithPath("optionGroup.required").type(BOOLEAN).description("옵션 필수 선택 여부"),
+                                        fieldWithPath("options").type(ARRAY).description("옵션 정보 리스트"),
+                                        fieldWithPath("options[].name").type(STRING).description("옵션 이름"),
+                                        fieldWithPath("options[].price").type(NUMBER).description("옵션 가격"),
+                                        fieldWithPath("options[].selected").type(BOOLEAN).description("옵션 선택 여부")
+                                )
+                        )
+                );
     }
 
     @Test
@@ -80,16 +111,27 @@ class OptionGroupControllerTest {
     @DisplayName("옵션 그룹 등록에 성공한다")
     void successRegisterOption() throws Exception {
         // given
-        OptionGroupRequest optionGroupRequest = new OptionGroupRequest("옵션 그룹 1", 10, false, 1L);
-        given(optionGroupService.registerOptionGroup(any(OptionGroup.class))).willReturn(1L);
-
+        Long storeId = 1L;
+        Long optionGroupId = 1L;
+        OptionGroupRequest optionGroupRequest = new OptionGroupRequest("옵션 그룹 1", 10, false, storeId);
+        given(optionGroupService.registerOptionGroup(any(OptionGroup.class))).willReturn(optionGroupId);
+        
         // when, then
         mvc.perform(post("/api/v1/option-groups")
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(optionGroupRequest)))
                 .andExpect(status().isCreated())
-                .andExpect(header().stringValues("Location", "/api/v1/option-groups/1"));
+                .andExpect(header().stringValues("Location", "/api/v1/option-groups/1"))
+                .andDo(
+                        document("option-group/save",
+                                requestFields(
+                                        fieldWithPath("name").type(STRING).description("옵션 그룹 이름"),
+                                        fieldWithPath("maxOptionCount").type(NUMBER).description("옵션 최대 선택 가능 개수"),
+                                        fieldWithPath("required").type(BOOLEAN).description("옵션 필수 선택 여부"),
+                                        fieldWithPath("storeId").type(NUMBER).description("옵션 그룹이 속한 가게 id")
+                                )
+                ));
     }
 
     @Test
@@ -97,15 +139,28 @@ class OptionGroupControllerTest {
     @DisplayName("옵션 그룹 수정에 성공한다")
     void successUpdateOption() throws Exception {
         // given
-        OptionUpdateRequest optionUpdateRequest = new OptionUpdateRequest("옵션 1", 500, true, 1L);
-        doNothing().when(optionGroupService).updateOptionGroup(any(OptionGroupUpdateInfo.class));
+        OptionGroupUpdateRequest optionGroupUpdateRequest = new OptionGroupUpdateRequest("옵션 1", 500, true);
+        doNothing().when(optionGroupService).updateOptionGroup(any(OptionGroupUpdateInfo.class), any(Long.class));
+        Long optionGroupId = 1L;
 
         // when, then
-        mvc.perform(put("/api/v1/option-groups")
+        mvc.perform(RestDocumentationRequestBuilders.put("/api/v1/option-groups/{optionGroupId}", optionGroupId)
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(optionUpdateRequest)))
-                .andExpect(status().isOk());
+                        .content(mapper.writeValueAsString(optionGroupUpdateRequest)))
+                .andExpect(status().isOk())
+                .andDo(
+                        document("option-group/update",
+                                pathParameters(
+                                        parameterWithName("optionGroupId").description("옵션 그룹 id")
+                                ),
+                                requestFields(
+                                        fieldWithPath("name").type(STRING).description("옵션 그룹 이름"),
+                                        fieldWithPath("maxOptionCount").type(NUMBER).description("옵션 최대 선택 가능 개수"),
+                                        fieldWithPath("required").type(BOOLEAN).description("옵션 필수 선택 여부")
+                                )
+                        )
+                );
     }
 
     @Test
@@ -114,12 +169,20 @@ class OptionGroupControllerTest {
     void successDeleteOption() throws Exception {
         // given
         doNothing().when(optionGroupService).deleteOptionGroup(any(Long.class));
+        Long optionGroupId = 1L;
 
         // when, then
-        mvc.perform(delete("/api/v1/option-groups")
-                        .param("optionGroupId", "1")
+        mvc.perform(RestDocumentationRequestBuilders.delete("/api/v1/option-groups/{optionGroupId}", optionGroupId)
                         .with(csrf()))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(
+                        document("option-group/delete",
+                                pathParameters(
+                                        parameterWithName("optionGroupId").description("옵션 그룹 id")
+                                )
+                        )
+                );
     }
 
 }
+
