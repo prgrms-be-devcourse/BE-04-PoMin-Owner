c7df2855a7a6bbe1983eee30752f7d3b9f0e62c7
junho <ssmm0205@naver.com>
2023-09-29 14:36:40 +0900

    feat: Menu와 OptionGroup 연관관계 설정하는 서비스 로직 작성

--- src/main/java/com/ray/pominowner/menu/service/MenuService.java
+++ src/main/java/com/ray/pominowner/menu/service/MenuService.java
@@ -1,8 +1,11 @@
 package com.ray.pominowner.menu.service;
 
 import com.ray.pominowner.menu.domain.Menu;
+import com.ray.pominowner.menu.domain.MenuOptionGroup;
+import com.ray.pominowner.menu.domain.OptionGroup;
 import com.ray.pominowner.menu.repository.MenuRepository;
 import com.ray.pominowner.global.vo.InfoSender;
+import com.ray.pominowner.menu.repository.OptionGroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,8 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class MenuService {
 
     private final MenuRepository menuRepository;
+    private final OptionGroupRepository optionGroupRepository;
     private final InfoSender infoSender;
-
 
     public Long registerMenu(Menu menu) {
         Long id = menuRepository.save(menu).getId();
@@ -30,6 +33,19 @@ public class MenuService {
         menuRepository.save(updatedMenu);
 
         infoSender.send(updatedMenu);
+    }
+
+    public void addOptionGroupToMenu(Long menuId, Long optionGroupId) {
+        Menu menu = menuRepository.findById(menuId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 메뉴가 존재하지 않습니다."));
+
+        OptionGroup optionGroup = optionGroupRepository.findById(optionGroupId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 옵션 그룹이 존재하지 않습니다."));
+
+        menu.addMenuOptionGroup(new MenuOptionGroup(menu, optionGroup));
+        menuRepository.save(menu);
+
+        infoSender.send(menu, optionGroup);
     }
 
 }
