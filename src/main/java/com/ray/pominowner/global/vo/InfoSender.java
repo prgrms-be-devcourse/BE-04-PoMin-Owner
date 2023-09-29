c56a8420f2774833326c1a6767c75aaf57dd853c
junho <ssmm0205@naver.com>
2023-09-29 14:36:02 +0900

    feat: 고객 서버로 메뉴에 옵션그룹 추가 시 보내는 기능 추가

--- src/main/java/com/ray/pominowner/global/vo/InfoSender.java
+++ src/main/java/com/ray/pominowner/global/vo/InfoSender.java
@@ -1,24 +1,29 @@
 package com.ray.pominowner.global.vo;
 
 import com.ray.pominowner.menu.domain.Menu;
+import com.ray.pominowner.menu.domain.OptionGroup;
 import com.ray.pominowner.menu.service.vo.MenuInfo;
+import com.ray.pominowner.menu.service.vo.OptionGroupInfoToSend;
 import com.ray.pominowner.store.domain.Store;
 import com.ray.pominowner.store.service.vo.StoreInfo;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import static org.springframework.http.HttpMethod.POST;
+
 @Component
 public class InfoSender {
+
+    private static final String URL_PREFIX = "http://52.78.144.166:8080/api/v1";
 
     public void send(Store store) {
         RestTemplate restTemplate = new RestTemplate();
         StoreInfo body = StoreInfo.from(store);
 
         HttpEntity<StoreInfo> storeInfoHttpEntity = new HttpEntity<>(body);
-        ResponseEntity<String> response = restTemplate.exchange("http://52.78.144.166:8080/api/v1/stores", HttpMethod.POST, storeInfoHttpEntity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(URL_PREFIX + "/stores", POST, storeInfoHttpEntity, String.class);
         validateResponse(response);
     }
 
@@ -27,8 +32,16 @@ public class InfoSender {
         MenuInfo body = MenuInfo.from(menu);
 
         HttpEntity<MenuInfo> menuInfoHttpEntity = new HttpEntity<>(body);
-        ResponseEntity<String> response = restTemplate.exchange("http://52.78.144.166:8080/api/v1/menus", HttpMethod.POST, menuInfoHttpEntity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(URL_PREFIX + "/menus", POST, menuInfoHttpEntity, String.class);
         validateResponse(response);
+    }
+
+    public void send(Menu menu, OptionGroup optionGroup) {
+        RestTemplate restTemplate = new RestTemplate();
+        OptionGroupInfoToSend body = OptionGroupInfoToSend.from(optionGroup, menu);
+
+        HttpEntity<OptionGroupInfoToSend> optionGroupInfoToSendHttpEntity = new HttpEntity<>(body);
+        restTemplate.exchange(URL_PREFIX + "/options", POST, optionGroupInfoToSendHttpEntity, String.class);
     }
 
     private void validateResponse(ResponseEntity<String> exchange) {
